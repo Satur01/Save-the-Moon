5cde946aba3fae13bd54b6919109e006f1199d3c
Ronald Pereira <ronaldpereira01@gmail.com>
2020-11-10 22:46:09 -0400

    added spaceship sprite and animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     Rigidbody2D rigid2d;
+    Animator animator;
+
     [SerializeField, Range(0, 100)]
     float speed = 0;
-    float moveHorizontal;
-    float moveVertical;
+
+    float moveHorizontal, moveVertical;
+
+    int velocityXHash;
+    int velocityYHash;
 
     void Start()
     {
         rigid2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        velocityXHash = Animator.StringToHash("Velocity X");
+        velocityYHash = Animator.StringToHash("Velocity Y");
     }
 
     void Update()
     {
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat(velocityXHash, Input.GetAxis("Horizontal"));
+        animator.SetFloat(velocityYHash, Input.GetAxis("Vertical"));
     }
 
     void FixedUpdate()
