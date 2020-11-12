91b6ef732a5a1d9b69d64b934cc1e90d2acbae6d
Ronald Pereira <ronaldpereira01@gmail.com>
2020-11-11 23:27:43 -0400

    added player laser shoots

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Vector2 shootDirection;
+    float shootSpeed;
+
+    Rigidbody2D rgb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y >= 10)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rgb2d.MovePosition((Vector2) transform.position + shootDirection * shootSpeed * Time.deltaTime);
+    }
+
+    public void SetBulletStats(Vector2 ShootDirection, float ShootSpeed)
+    {
+        shootDirection = ShootDirection;
+        shootSpeed = ShootSpeed;
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Vector2 ShootDirection;
+    public float CreationRate;
+
+    [Range(1, 100)]
+    public float ShootSpeed = 10;
+
+    GameObject bullet;
+    float time;
+
+    void Start()
+    {
+        time = Time.time + CreationRate;
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Fire1") > 0)
+        {
+            bullet = ObjectPool.SharedInstance.GetPooledObject();
+
+            if (bullet != null && Time.time > time)
+            {
+                bullet.transform.position = this.transform.position;
+                
+                bullet.GetComponent<Bullet>().SetBulletStats(ShootDirection, ShootSpeed);
+                bullet.SetActive(true);                            
+                time = Time.time + CreationRate;
+            }
+        }
+
+
+    }
+}
