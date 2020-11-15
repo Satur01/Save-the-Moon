e441447f0292801ba5d1693c9df38b6abf7975fb
Ronald Pereira <ronaldpereira01@gmail.com>
2020-11-15 14:00:24 -0400

    modify scripts folder structure / added player and enemy scriptable objects

---
+++ Assets/Scripts/Scriptable/EnemyScriptableData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy", menuName = "ScriptableData")]
+public class EnemyScriptableData : ScriptableObject
+{
+    int life;
+    int damage;
+    int movementSpeed;
+    float shootRate;
+    Sprite enemySprite;
+}
---
+++ Assets/Scripts/Scriptable/PlayerScriptableData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player", menuName = "ScriptableData")]
+public class PlayerScriptableData : ScriptableObject
+{
+    int life;
+    int damage;
+    int movementSpeed;
+    float shootRate;
+    Sprite playerSprite;
+}
